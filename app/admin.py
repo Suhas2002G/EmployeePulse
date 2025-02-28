96f243dad473fadc5ef85d05f9994d25c7b92e01
Suhas Gondukupi <suhas8838@gmail.com>
2025-02-28 22:38:32 +0530

    Add Model Classes

--- app/admin.py
+++ app/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from .models import Dept
-
+from .models import Dept, Role
 
 class DeptAdmin(admin.ModelAdmin):
     list_display = ['dept_id','dept_name','desc','status', 'created_at', 'updated_at']
     list_filter = ['dept_name','status']
     search_fields = ['dept_name']
-
+    
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ['uid','role','desc','created_at','updated_at']
+    list_filter = ['uid','role']
+    search_fields = ['uid','role']
 
 admin.site.register(Dept,DeptAdmin)
+admin.site.register(Role,RoleAdmin)
--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
-
-# Department Model
+# Department Model : 
 class Dept(models.Model):
     CAT = (('IT','IT'),('Support','Support'),('Sales','Sales'))
     dept_id = models.BigAutoField(primary_key=True)
@@ -14,3 +15,28 @@ class Dept(models.Model):
     def __str__(self):
         return self.dept_name
 
+
+# Role Model : 
+class Role(models.Model):
+    uid = models.ForeignKey(User, db_column='uid', on_delete=models.CASCADE)
+    ROLE=(('Admin','Admin'),('Manager','Manager'),('Team Lead','Team Lead'),('Employee','Employee'))
+    role = models.CharField(max_length=100, choices=ROLE)
+    desc = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)  
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.uid.username} : {self.role}'
+
+
+
+# Employee : 
+class Employee(models.Model):
+    uid = models.ForeignKey(User, db_column='uid', on_delete=models.CASCADE)
+    mobile = models.CharField(max_length=10)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    dept = models.ForeignKey(Dept, on_delete=models.CASCADE)
+    doj = models.DateField(auto_now_add=True)
+#fname, lname,email, username, password will be done by Auth-User
+    def __str__(self):
+        return f'{self.uid.username} : {self.dept} - {self.role}'
