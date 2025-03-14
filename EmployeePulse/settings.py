d9f26a9cea5f2a1c1caaf2ccd9e7538087975ac3
Suhas Gondukupi <suhas8838@gmail.com>
2025-03-14 12:35:15 +0530

    add requirements.txt

--- EmployeePulse/settings.py
+++ EmployeePulse/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -150,6 +151,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 MEDIA_URL='/media/'      #media url
 MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
